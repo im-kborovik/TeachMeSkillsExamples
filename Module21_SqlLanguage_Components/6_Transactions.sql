10f813e36d31408a26b66023cee940f5b844feeb
Konstantin.Borovik <konstantin.borovik@fortegrp.com>
2022-10-07 16:00:30 +0300

    - add transactions

---
+++ Module21_SqlLanguage_Components/6_Transactions.sql
@@ -0,0 +1,37 @@
+﻿create database Module20_Transactions;
+go
+use Module20_Transactions;
+go
+
+create table Products
+(
+    Id           int           not null identity primary key,
+    Name         nvarchar(100) not null,
+    Manufacturer nvarchar(100) not null,
+    Quantity     int           not null default 0,
+    Price        money         not null,
+);
+go
+
+begin transaction
+
+insert into Products
+values ('iPhone 6', 'Apple', 2, 36000),
+       ('iPhone 6S', 'Apple', 2, 41000),
+       ('iPhone 7', 'Apple', 5, 52000);
+
+select * from Products;
+
+insert into Products
+values ('Galaxy S8', 'Samsung', 2, 46000),
+       ('Galaxy S8 Plus', 'Samsung', 1, 56000),
+       ('Mi 5X', 'Xiaomi', 2, 26000),
+       ('OnePlus 5', 'OnePlus', 6, 38000);
+
+select * from Products;
+
+rollback
+
+-- commit
+
+select * from Products;
