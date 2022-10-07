fef102aafcc364833182c8a8a24c13e23dabe6bd
Konstantin.Borovik <konstantin.borovik@fortegrp.com>
2022-10-07 12:03:44 +0300

    - add indexes part

---
+++ Module21_SqlLanguage_Components/5_Indexes.sql
@@ -0,0 +1,31 @@
+﻿create database Module20_Indexes;
+go
+use Module20_Indexes;
+go
+
+create table Products
+(
+    Id           int           not null identity primary key,
+    Name         nvarchar(100) not null,
+    Manufacturer nvarchar(100) not null,
+    Quantity     int           not null default 0,
+    Price        money         not null,
+);
+go
+
+create unique nonclustered index IX_Name_Manufacturer_Unique
+on Products(Name, Manufacturer)
+go
+
+insert into Products
+values ('iPhone 6', 'Apple', 2, 36000),
+       ('iPhone 6S', 'Apple', 2, 41000),
+       ('iPhone 7', 'Apple', 5, 52000),
+       ('Galaxy S8', 'Samsung', 2, 46000),
+       ('Galaxy S8 Plus', 'Samsung', 1, 56000),
+       ('Mi 5X', 'Xiaomi', 2, 26000),
+       ('OnePlus 5', 'OnePlus', 6, 38000)
+go
+
+insert into Products
+values ('iPhone 6', 'Apple', 2, 30000) -- тут будет ошибка
