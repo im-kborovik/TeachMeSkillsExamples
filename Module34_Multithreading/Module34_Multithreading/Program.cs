10ecc48a11e8be8a9facf86f45baad79a53c54f8
Konstantin Borovik <kes8594@yandex.ru>
2022-10-21 00:38:35 +0300

    - add multithreading first part

---
+++ Module34_Multithreading/Module34_Multithreading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Module34_Multithreading
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var threadBasic = new ThreadBasic();
+            threadBasic.SimpleThreadExample();
+            threadBasic.ParameterizedThreads();
+            threadBasic.ThreadProblems();
+        }
+    }
+}
---
+++ Module34_Multithreading/Module34_Multithreading/ThreadBasic.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Module34_Multithreading
+{
+    public class ThreadBasic
+    {
+        public void SimpleThreadExample()
+        {
+            var thread1 = new Thread(Print);
+            thread1.Start();
+            var thread2 = new Thread(() => Console.WriteLine("Here is lambda approach"));
+            thread2.Start();
+
+            Console.WriteLine("Here is main thread finished");
+            
+            Thread.Sleep(5000);
+        }
+
+        public void ParameterizedThreads()
+        {
+            var thread = new Thread(MethodWithParameter);
+            thread.Start(nameof(ParameterizedThreads));
+        }
+
+        public void ThreadProblems()
+        {
+            var state = new State();
+
+            for (var i = 0; i < 20; i++)
+            {
+                Task.Run(() => new StateWrapper().RunInvestigation(state));
+            }
+            
+            Thread.Sleep(5000);
+        }
+
+        private void Print()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Hey! Here is method approach of threading 2 seconds soon");
+        }
+
+        private void MethodWithParameter(object obj)
+        {
+            if (obj is string str)
+            {
+                Console.WriteLine($"Here is string with value: {str}");
+            }
+        }
+
+        private class StateWrapper
+        {
+            public void RunInvestigation(State state)
+            {
+                Console.WriteLine("Start new task.");
+
+                var iterator = 0;
+                while (true)
+                {
+                    state.ChangeState(iterator++);
+                }
+            }
+        }
+
+        private class State
+        {
+            private int _state;
+
+            public void ChangeState(int iteration)
+            {
+                if (_state == 0)
+                {
+                    _state++;
+                    if (_state != 1)
+                    {
+                        Console.WriteLine($"Thread competition is appeared on {iteration} iteration");
+                        throw new Exception("Thread competition is finished");
+                    }
+                }
+
+                _state = 0;
+            }
+        }
+    }
+}
