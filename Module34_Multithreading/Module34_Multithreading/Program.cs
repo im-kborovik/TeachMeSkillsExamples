2f2341e1f9dc821aaf0b42481b5211d322c08534
Konstantin.Borovik <konstantin.borovik@fortegrp.com>
2022-10-21 18:16:22 +0300

    - add second part of threads

--- Module34_Multithreading/Module34_Multithreading/Program.cs
+++ Module34_Multithreading/Module34_Multithreading/Program.cs
@@ -7,9 +7,15 @@ namespace Module34_Multithreading
         private static void Main(string[] args)
         {
             var threadBasic = new ThreadBasic();
-            threadBasic.SimpleThreadExample();
-            threadBasic.ParameterizedThreads();
-            threadBasic.ThreadProblems();
+            // threadBasic.SimpleThreadExample();
+            // threadBasic.ParameterizedThreads();
+            
+            // threadBasic.ThreadProblems((stateWrapper, state) => stateWrapper.RunInvestigation(() => state.ChangeStateWithoutLocking));
+            // threadBasic.ThreadProblems((q, w) => q.RunInvestigation(() => w.ChangeStateSyncByLock));
+            // threadBasic.ThreadProblems((q, w) => q.RunInvestigation(() => w.ChangeStateSyncByMonitor));
+            // threadBasic.ThreadProblems((q, w) => q.RunInvestigation(() => w.ChangeStateSyncByMutex));
+            // threadBasic.ThreadProblems((q, w) => q.RunInvestigation(() => w.ChangeStateSyncBySemaphore));
+            threadBasic.ThreadProblems((q, w) => q.RunInvestigation(() => w.ChangeStateSyncByAutoResetEvent));
         }
     }
 }
